c7349830707ae08a4418b911bf0788363bea07b3
Shehbaz <mdshehbaz666666@gmail.com>
2023-11-15 00:08:16 +0530

    v7_RestApi

---
+++ models/Products_schema.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+
+const ProductSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    Prise:{
+        type:Number,
+        required:[true,"Price must be provided"]
+    },
+    featured:{
+        type:Boolean,
+        default:false,
+    },
+    rating:{
+        type:Number,
+        default:4.9
+    },
+    createdAt:{
+        type:Date,
+        default:Date.now(),
+    },
+    company:{
+        type:String,
+        enum:{
+            values : ["Apple","Amazon","Microsoft","Dell","UrbanMonkey"],
+            message: `{VALUE} is not supported`
+        }
+
+    }
+})
+
+
+module.exports = mongoose.model("Product",ProductSchema);
